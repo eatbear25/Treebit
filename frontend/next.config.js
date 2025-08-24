7ac180d305fbd1b7614b2f76d27c7b95cfbc7677
eatbear25 <eatbear25@gmail.com>
2025-08-25 01:34:03 +0800

    chore: 更改 next.config 設定

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,11 +2,6 @@
 const nextConfig = {
   // 關閉React Strict Mode工具(避免useEffect執行兩次)
   reactStrictMode: false,
-  // eslint設定
-  eslint: {
-    // 警告: 開啟以下的設定將會忽略所有在build時的eslint錯誤與警告，不建議在部署時直接使用，或請先自行修正eslint相關錯誤與警告
-    ignoreDuringBuilds: true,
-  },
   // sass設定，修正新版本sass導致的過多棄用警告訊息
   sassOptions: {
     silenceDeprecations: ['legacy-js-api'],
@@ -29,7 +24,10 @@ const nextConfig = {
     return [
       {
         source: '/api/:path*',
-        destination: 'https://treebit-backend.vercel.app/api/:path*',
+        destination:
+          process.env.NODE_ENV === 'production'
+            ? 'https://treebit-backend.vercel.app/api/:path*'
+            : 'http://localhost:3002/api/:path*',
       },
     ]
   },
